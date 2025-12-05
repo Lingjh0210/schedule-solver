f0b226f110e7b996f7912592d7fd8fd5a87845e3
Lingjh0210 <78132372+Lingjh0210@users.noreply.github.com>
2025-12-05 10:58:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -930,7 +930,9 @@ def calculate_smart_defaults(packages, subject_hours, default_concurrency=1):
         raw_min = math.floor(min_student_count / current_max_classes)
         suggested_min_size = max(1, raw_min - 2)
     else:
-        suggested_min_size = max(1, min_student_count-3)
+        suggested_min_size = max(1, min_student_count-2)
+    else:
+        suggested_new_size = min_student_count
 
 
     max_student_count = max(enrollment.values())
